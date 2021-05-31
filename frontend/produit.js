5ae681703a2721af4d4a37c91cceee5967c82302
git504 <git504@mailo.com>
2021-05-31 12:02:19 +0200

    JS

--- frontend/produit.js
+++ frontend/produit.js
@@ -2,7 +2,7 @@
 const url = "http://localhost:3000/api/cameras";
 const myurl = window.location.href; 
 const searchParams = new URLSearchParams(myurl);
-let camId = params.get("id");
+let camId = searchParams.get("id");
 
 /*Appel du produit séléctionné*/
 let mesVariables; //On stock les données du produit dans cette variable.
@@ -11,6 +11,10 @@ async function selectionProduit() {
     fetch(url + "/" + camId).then(function(response){
         response.json().then(function(data){
             mesVariables = data;
+        })
+    })
+}
+selectionProduit();
 
 //On recupere dans le .json que les elements qui nous interessent;
 //On match l'ID  qui correspond a la selection;
