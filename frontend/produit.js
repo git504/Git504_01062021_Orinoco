57bbfcc437d5d0c8eebe2850d1fdf3389881512f
git504 <git504@mailo.com>
2021-05-31 09:54:11 +0200

    JS

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,3 +1,9 @@
+
+// API
+const url = "http://localhost:3000/api/cameras";
+const urlOrder = "http://localhost:3000/api/cameras/order";
+
+/*Appel de l'API*/
 const getCams = function () {
     return fetch (url)
     .then(function(response){
