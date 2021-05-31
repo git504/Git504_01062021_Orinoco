360897fa6a9b41f089b236812aedf3b8f3475d69
git504 <git504@mailo.com>
2021-05-31 11:57:55 +0200

    JS

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,40 +1,27 @@
 // API
 const url = "http://localhost:3000/api/cameras";
+const myurl = window.location.href; 
+const searchParams = new URLSearchParams(myurl);
+let camId = params.get("id");
 
-//Appel de l'API
-const getProduit = function () {
-    return fetch ("http://localhost:3000/api/cameras")
-    //.then((response) => response.json())
-    .then(function(response){
-        let parseCam = JSON.parse("http://localhost:3000/api/cameras");
-    })
-    .then(function(response){
-        const myurl = window.location.href; 
-        //Url complète de la page en cours de consultation
-        //La propriété est accessible en écriture et permet de réaliser une redirection par programmation.
-        const searchParams = new URLSearchParams(myurl)
-        //Lit les params de l'URL
-        for (let p of searchParams) {
-            const id = p[1]
-            console.log(id);
-          }
-        //On isole l'élément numéro 1 dans l'array const ID
-        console.log(searchParams.get("id"))
-    })
-    .catch(error => alert("Erreur : " + error));
-}
-getProduit();
+/*Appel du produit séléctionné*/
+let mesVariables; //On stock les données du produit dans cette variable.
+
+async function selectionProduit() {
+    fetch(url + "/" + camId).then(function(response){
+        response.json().then(function(data){
+            mesVariables = data;
 
 //On recupere dans le .json que les elements qui nous interessent;
 //On match l'ID  qui correspond a la selection;
 
-const findElement = function (data, _id) {
-for (let _id of data) {
-if (_id === _id) {
- return true;
- }{
- return false;
- }
-}
-}
-console.log(findElement);
+// const findElement = function (data, _id) {
+// for (let _id of data) {
+// if (_id === _id) {
+//  return true;
+//  }{
+//  return false;
+//  }
+// }
+// }
+// console.log(findElement);
