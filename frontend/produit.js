55b3ae3e47f500195f8ff3404f734ed460f9d863
git504 <git504@mailo.com>
2021-05-31 11:04:53 +0200

    JS

--- frontend/produit.js
+++ frontend/produit.js
@@ -25,8 +25,8 @@ getProduit();
 //On recupere dans le .json que les elements qui nous interessent;
 //On match l'ID  qui correspond a la selection;
 
-const findElement = (data, "_id") {
-for (let "_id" of data) {
+const findElement = function (data, _id) {
+for (let _id of data) {
 if ("_id" === "_id") {
  return true;
  }{
