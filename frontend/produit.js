58263ee5bdbd692c3aff5ff08e565e6211fc2540
git504 <git504@mailo.com>
2021-05-31 10:13:39 +0200

    JS

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,18 +1,17 @@
 // API
 const url = "http://localhost:3000/api/cameras";
 const urlOrder = "http://localhost:3000/api/cameras/order";
-const myurl = window.location.href //prend l'url de la page produit
-const id = myurl.searchParams.get("id") //on recupere un parametre precis id
-console.log(id);
+
 
 /*Appel de l'API*/
+const url = "http://localhost:3000/api/cameras";
 const getProduit = function () {
     return fetch (url)
+    .then((response) => response.json())
     .then(function(response){
-        return response.json()
-    })
-    .then(function(resp){
-        return response.json()
+        const myurl = window.location.href;
+        const id = myurl.searchParams.get("id");
+        console.log(id)
     })
     .catch(error => alert("Erreur : " + error));
 }
