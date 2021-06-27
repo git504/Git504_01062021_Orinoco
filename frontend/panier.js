401785a5831ecd0d4a9cfd00cfc0d8b80b5dde98
git504 <git504@mailo.com>
2021-06-27 18:33:03 +0200

    js

--- frontend/panier.js
+++ frontend/panier.js
@@ -20,7 +20,7 @@ console.log("hello");
 /*Fonction de suppression d'article du panier*/
 function suppressionArticle(i) {
   console.log("suppression article i :", i);
-  panier.splice(i, 1); //suppression de l'element i du tableau;
+  panier.splice(i, 1); //suppression --> 1 de l'element i du tableau;
   localStorage.clear(); //on vide le storage avant de le mettre à jour;
   localStorage.setItem("monPanier", JSON.stringify(panier)); //maj du panier sans l'élément i;
   window.location.reload();
@@ -36,80 +36,28 @@ function affichagePanier() {
 
     let listOfBag = "";
     /*On crée l'affichage de la liste des produits proposés qui sera présente sur l'index*/
-    panier.map((article) => {
+    panier.map((article, index) => {
       listOfBag += `
 <tr>
-<td ><img id="articleImage">${article.imgUrl}</td>
+<td ><img id="articleImage" src="${article.imgUrl}"></td>
 <td>${article.name}</td>
 <td>${article.lense}</td>
 <td>${article.quantity}</td>
-<td>${article.price}</td>
-<td class="fas fa-trash-alt"></td>
+<td>${article.price / 100}</td>
+<td class="fas fa-trash-alt" onclick="suppressionArticle(${index})"></td>
 </tr>`;
     });
-
-    // //Création du tableau
-    // let tableauPanier = create("table", "class", "tableauPanier");
-    // let tableauHeaderLigne = create("tr", "class", "tableauHeaderLigne");
-    // let tableauHeaderImage = document.createElement("th");
-    // let tableauHeaderNom = document.createElement("th");
-    // let tableauHeaderPrix = document.createElement("th");
-    // let tableauHeaderAction = document.createElement("th");
-    // let tableauFooterLigne = create("tr", "class", "tableauFooterLigne");
-    // let tableauFooterPrixTotal = create("th", "class", "tableauFooterPrixTotal");
-
-    // /*Attributs suplémentaires*/
-    // tableauFooterPrixTotal.setAttribute("colspan", "4");
-
-    // /*Hiérarchisation des élements crées*/
-    // tableauSection.appendChild(tableauPanier);
-    // tableauPanier.appendChild(tableauHeaderLigne);
-    // tableauHeaderLigne.appendChild(tableauHeaderImage);
-    // tableauHeaderLigne.appendChild(tableauHeaderNom);
-    // tableauHeaderLigne.appendChild(tableauHeaderPrix);
-    // tableauHeaderLigne.appendChild(tableauHeaderAction);
-
-    // /*Attribution des données aux élements créees*/
-    // tableauHeaderImage.textContent = "Article(s)";
-    // tableauHeaderNom.textContent = "Nom(s)";
-    // tableauHeaderPrix.textContent = "Prix";
-    // tableauHeaderAction.textContent = "Action";
-
-    /*Création d'une ligne dans le tableau pour chaque produit composant le panier*/
-    JSON.parse(localStorage.getItem("monPanier")).forEach((article, index) => {
-      // let articleLigne = create("tr", "id", "articleLigne");
-      // let articleImage = create("img", "id", "articleImage");
-      // let articleNom = create("td", "id", "articleNom");
-      // let articlePrix = create("td", "id", "articlePrix");
-      // let articleAction = create("i", "id", index);
-      /*Attributs suplémentaires*/
-      // articleImage.setAttribute("src", article.imageUrl);
-      // articleAction.setAttribute("alt", "Retirer l'article du panier.");
-      // articleAction.setAttribute("class", "fas fa-trash-alt"); //Logo poubelle pour supprimer l'article du panier.
-      /*Suppression de l'article en cliquant sur la poubelle*/
-      // articleAction.addEventListener("click", function(event){
-      //     suppressionArticle(event.target.id);
-      // });
-      /*Hiérarchisation des élements crées*/
-      // tableauPanier.appendChild(articleLigne);
-      // articleLigne.appendChild(articleImage);
-      // articleLigne.appendChild(articleNom);
-      // articleLigne.appendChild(articlePrix);
-      // articleLigne.appendChild(articleAction);
-      /*Attribution des données aux élements créees*/
-      //     articleNom.textContent = article.name;
-      //     articlePrix.textContent = article.price / 100 + " " + "euros";
-    });
+    tableauSection.innerHTML = listOfBag;
 
     /*Création de la ligne du bas du tableau affichant le prix total de la commande*/
-    // tableauPanier.appendChild(tableauFooterLigne);
-    // tableauFooterLigne.appendChild(tableauFooterPrixTotal);
 
-    // JSON.parse(localStorage.getItem("monPanier")).forEach(priceArticle => {
-    // total += priceArticle.price / 100;
-    // });
+    JSON.parse(localStorage.getItem("monPanier")).forEach((priceArticle) => {
+      total += priceArticle.price / 100;
+    });
 
-    // tableauFooterPrixTotal.textContent = "Prix total: " + total + " euros";
+    tableauFooterPrixTotal = document.querySelector(".tableauFooterLigne");
+    
+    tableauFooterPrixTotal.textContent = "Prix total: " + total + " euros";
   }
 }
 affichagePanier();
